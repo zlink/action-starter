7d84bca90cf798b8e89edd04054a62f7ebe5f676
ElrickZhao <elrickzhao@yunify.com>
2020-11-19 15:28:09 +0800

    update

---
+++ tests/RequestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+
+  public function testSomeRequest()
+  {
+    $this->assertTrue(true, 'This should be already working.');
+  }
+
+  public function testString()
+  {
+    $this->assertTrue(true, 'This should be already working.');
+  }
+
+}
